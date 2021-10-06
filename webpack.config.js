188ffbd6cff2e06aa89481e5335e16a9600e88d4
DIEGO CARNAVALLI BOLSONI <dibolsoni@gmail.com>
2021-10-06 13:37:34 -0300

    added more loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
 
 
 const buildPath = './build';
@@ -13,18 +15,72 @@ module.exports = {
         path: path.resolve(__dirname, buildPath),
         filename: '[name].[fullhash].js'
     },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            use: 'babel-loader',
-            exclude: path.resolve(__dirname, './node_modules/')
-        }, {
-            test: /\.(jpe?g|png|gif|svg|tga|glb|babylon|mtl|pcb|pcd|prwm|obj|mat|mp3|ogg)$/i,
-            use: 'file-loader',
-            exclude: path.resolve(__dirname, './node_modules/')
-        }]
+    module:
+    {
+        rules:
+        [
+            // HTML
+            {
+                test: /\.(html)$/,
+                use: ['html-loader']
+            },
+
+            // JS
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:
+                [
+                    'babel-loader'
+                ]
+            },
+
+            // CSS
+            {
+                test: /\.css$/,
+                use:
+                [
+                    MiniCSSExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+
+            // Images
+            {
+                test: /\.(jpg|png|gif|svg)$/,
+                use:
+                [
+                    {
+                        loader: 'file-loader',
+                        options:
+                        {
+                            outputPath: 'assets/images/'
+                        }
+                    }
+                ]
+            },
+
+            // Fonts
+            {
+                test: /\.(ttf|eot|woff|woff2)$/,
+                use:
+                [
+                    {
+                        loader: 'file-loader',
+                        options:
+                        {
+                            outputPath: 'assets/fonts/'
+                        }
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
-        new HtmlWebpackPlugin({ 'title': 'Pong in ThreeJS' })
+        new HtmlWebpackPlugin({ 
+            template: path.resolve(__dirname, './index.html'),
+            minify: true
+        }),
+        new MiniCSSExtractPlugin()
     ]
 };
