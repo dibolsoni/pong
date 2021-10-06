263bb70c00f7677b53756f05677b138e43fca2db
DIEGO CARNAVALLI BOLSONI <dibolsoni@gmail.com>
2021-10-06 13:36:39 -0300

    added debug gui

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import WebGL from './src/WebGL';
 import main from './src/main';
+import * as dat from 'dat.gui';
+import './index.css';
+
 
 
 if (WebGL.isWebGLAvailable()) {
     console.log('#WebGL# loaded');
-    main();
+    const gui = new dat.GUI();
+    const dom = document.querySelector('canvas.webgl');
+    main(dom);
 } else {
     const warning = WebGL.getWebGLErrorMessage();
     document.appendChild(warning);
