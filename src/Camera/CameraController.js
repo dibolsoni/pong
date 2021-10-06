83ab971a72a0aa157e70d6fc8557b04c9cdcf592
DIEGO CARNAVALLI BOLSONI <dibolsoni@gmail.com>
2021-10-06 14:49:21 -0300

    constructor and reset state done - compiling and passing

--- src/Camera/CameraController.js
+++ src/Camera/CameraController.js
@@ -1,24 +1,24 @@
 import { PerspectiveCamera, Vector3 } from "three";
 
-export default class CameraController extends PerspectiveCamera{
-    constructor(fov=75, aspect=2, near=0.1, far=5){
+export default class CameraController extends PerspectiveCamera {
+    #default_values;
+    constructor(state = { fov: 75, aspect: 2, near: 0.1, far: 5 }) {
+        const { fov, aspect, near, far } = state;
         super(fov, aspect, near, far);
-        this.state = {fov, aspect, near, far};
+        this.default_values = state;
     }
 
     reset_state() {
-        this.state = {
-            fov: 75,
-            aspect: 2,
-            near: 0.1,
-            far: 5,
-        };
+        for (const key in this.default_values) {
+            const value = this.default_values[key];
+            this[key] = value;
+        }
     }
 
 
-    move_left(velocity_seconds = 0.001){
+    move_horizontal(velocity_seconds = 0.001) {
         const cur = this.position.x;
-        this.position.setX(cur - velocity_seconds);
+        this.position.setX(cur + velocity_seconds);
         this.updateProjectionMatrix();
     }
 
--- src/Camera/CameraController.test.js
+++ src/Camera/CameraController.test.js
@@ -1,7 +1,7 @@
 import CameraController from "./CameraController";
 
 
-describe('testing a camera', () => {
+describe('a camera', () => {
     const camera = new CameraController();
     const defaults = {
         fov: 75,
@@ -9,20 +9,32 @@ describe('testing a camera', () => {
         near: 0.1,
         far: 5
     };
-    it('initiate with default values', () => {
+    it('initiates with default values', () => {
         for (const key in defaults) {
-            if (Object.hasOwnProperty.call(defaults, key)) {
+            if (Object.hasOwnProperty.call(camera, key)) {
                 expect(camera[key]).toBe(defaults[key]);
                 expect(camera[key] + 1).not.toBe(defaults[key]);
             }
         }
     });
 
-    it('left moves', () => {
+    it('resets to defaults values', () => {
+        camera.fov = 48;
+        camera.reset_state();
+        expect(camera.fov).toBe(defaults.fov);
+
+    });
+
+    it('linear left/right moves', () => {
         const old_pos = camera.position.x;
         const expected = old_pos - 1;
-        camera.move_left(1);
+        camera.move_horizontal(-1); 
         expect(camera.position.x).toBe(expected);
-    })
+        camera.move_horizontal(+1);
+        expect(camera.position.x).toBe(old_pos);
+    });
+
+
+
 
 });
