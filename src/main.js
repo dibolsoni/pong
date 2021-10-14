6b6ce6b1065f5db241b34dc3989d1020ef7fbdba
dibolsoni <dibolsoni@gmail.com>
2021-10-14 18:14:35 -0300

    start world

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import World from './World/World';
+import {enableMapSet} from "immer"
+
+enableMapSet()
 
 function main(dom) {
     if (dom.tagName.toUpperCase() !== 'CANVAS')
         throw Error("can't initiate without a canvas");
     const canvas = dom;
     const world = new World(canvas);
+    world.start();
 }
 
 export default main;
