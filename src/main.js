3b388813fb2ddd58f8f7edb82ecbd33d9ebd8a0e
dibolsoni <dibolsoni@gmail.com>
2021-10-25 21:12:02 -0300

    rm immer

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import World from './World/World';
-import {enableMapSet} from "immer"
 import * as dat from 'dat.gui';
 
 
 function main() {
-    enableMapSet() //eslint-disable-line no-eval
     const gui = new dat.GUI();
     const canvas = document.querySelector('canvas.webgl', gui);
     const world = new World({canvas});
